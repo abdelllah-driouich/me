2be3869e0b813cd8f32665cbf2302dae7206f7cc
Parabeac-Nest <admin@parabeac.com>
2022-11-20 21:53:20 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/testingthischet_colors.g.dart
@@ -0,0 +1,74 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TestingthischetColors {
+
+static const Color uIGray20 = Color(0xfff8f7fa);
+
+static const Color uIGray80 = Color(0xff2c2c2c);
+
+static const Color uIGray40 = Color(0xffa8a8a8);
+
+static const Color primaryOrange = Color(0xffeeac5c);
+
+static const Color accentsRed = Color(0xfffe8668);
+
+static const Color uIGray100 = Color(0xff1e1e1e);
+
+static const Color uIGray60 = Color(0xff494949);
+
+static const Color primaryBrown2 = Color(0xffd3a888);
+
+static const Color textGray40 = Color(0xffcbcbd4);
+
+static const Color textGray60 = Color(0xffa6a6aa);
+
+static const Color primaryBrown3 = Color(0xffd2ae82);
+
+static const Color textGray100 = Color(0xff000000);
+
+static const Color secondaryGreen1 = Color(0xff00731f);
+
+static const Color secondaryGreen2 = Color(0xff008821);
+
+static const Color secondaryGreen3 = Color(0xff00a929);
+
+static const Color secondaryBlue = Color(0xff0082a2);
+
+static const Color secondaryBlue2 = Color(0xff99cedd);
+
+static const Color primaryBrown = Color(0xff846046);
+
+static const Color accentsOrange = Color(0xffffce59);
+
+static const Color accentsTeal = Color(0xff30d6b0);
+
+static const Color accentsPurple = Color(0xff8d007c);
+
+static const Color uIWhite = Color(0xffffffff);
+
+static const Color backgroundWhite = Color(0xffffffff);
+
+static const Color backgroundGray20 = Color(0xfff8f7fa);
+
+static const Color textGray80 = Color(0xff3a3a3a);
+
+static const Color textWhite = Color(0xffffffff);
+
+static const Color textYellow = Color(0xffffe202);
+
+static const Color textBlue = Color(0xff5fa1d5);
+
+static const Color textGreen = Color(0xff51b960);
+
+static const Color textRed = Color(0xfffe8668);
+
+static const Color errorRed = Color(0xfff61a3d);
+
+static const Color successGreen = Color(0xff008821);
+}
---
+++ lib/theme/testingthischet_text_styles.g.dart
@@ -0,0 +1,200 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TestingthischetTextStyles {
+
+static const TextStyle headingH516px|24px = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH140px|40px = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textLabel12px|12px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH328px|32px = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH420px|24px = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH612px|12px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headingH232px|36px = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textBodyLg20px|24px = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textBodyReg16px|20px = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textBodySm12px|16px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textBodySmBold12px|16px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textHint10px|16px = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsLinkReg16px|20px = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.underline,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsLinkSm12px|16px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsStrikethroughReg16px|16px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsStrikethroughSm12px|12px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsPillReg16px|16px = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle interactionsPillSm12px|12px = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w800,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle textAltBoldHint10px|16px = TextStyle(
+  fontSize: 8,
+  fontWeight: FontWeight.w900,
+  letterSpacing: 0,
+  fontFamily: 'Lato',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
